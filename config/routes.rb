ccf1f149fe2dadd3de46b23c31113911cc75fadd
Johanna Brewer <johannab@gmail.com>
2022-08-31 04:59:18 -0400

    Obliterates existing app except notes folder and README. Creates new Rails 7 app scaffold in its place to begin process of updating base repo.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  root to: "stations#index"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
 end
